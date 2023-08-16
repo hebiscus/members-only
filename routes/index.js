1e43729da2b67e077d4947633642da0b6b913cbb
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-16 20:40:54 +0200

    edit get post homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Members Only', user: req.user, failLogin: req.session.message });
 });
+
+router.post('/', passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/", failureMessage: true
+}));
 
 module.exports = router;
