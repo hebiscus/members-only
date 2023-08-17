df59ce0bcdc741ba2a702146c8ad97baa1037db6
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-17 12:39:51 +0200

    display error message on unsuccessfull login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Members Only', user: req.user, failLogin: req.session.message });
+  res.render('index', { title: 'Members Only', user: req.user, message: req.session.messages });
 });
 
 router.post('/', passport.authenticate("local", {
