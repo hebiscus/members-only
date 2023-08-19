343ec3e3b8e572acbcf54a8c07a30fc5f95d0eee
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-19 20:02:41 +0200

    fix username exists validation, delete unused import

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,32 +3,26 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 
 exports.signup = [
-    body("firstName").isString().trim().exists({values: [null, "falsy"]}).withMessage("hey, first name is undefined!").isLength({min: 2, max: 20}).withMessage("between 2-20 letters"),
+    body("firstName").isString().trim().exists({values: [null, "falsy"]}).withMessage("hey, first name is undefined!").isLength({min: 2, max: 20}).withMessage("name must be between 2-20 letters"),
     body("lastName").isString().trim(),
-    body("username").isEmail().withMessage("you sure that's an email?").custom(async value => {
-        try {
+    body("username").isEmail().withMessage("you sure that's a valid email?").custom(async value => {
             const existingUser = await User.find({username: value});
-            if (existingUser) {
+            if (existingUser.length !== 0) {
                 throw new Error("Wait...you're already signed up!");
-            }
-        } catch(err) {
-            console.log(err)
-        }
-    }).withMessage("you're already signed up"),
+            }}),
     body("password").isLength({min: 5, max: 20}).withMessage("password has to be between 5 and 20 symbols"),
     body("passwordConf").custom((value, { req }) => {
         return value === req.body.password;
       }).withMessage("passwords don't match!"),
     
     (async (req, res, next) => {
-        console.log(body)
         const errors = validationResult(req);
+        console.log(errors)
         if(!errors.isEmpty()) {
             return res.render("sign-up", { title: "Sign-Up", errors: errors.array() });
         }
         const salt =  bcrypt.genSaltSync(10);
         const hashed = bcrypt.hashSync(req.body.password, salt);
-        console.log(hashed)
         try {
             const newUser = new User({firstName: req.body.firstName, lastName: req.body.lastName, username: req.body.username, password: hashed})
             await newUser.save();
